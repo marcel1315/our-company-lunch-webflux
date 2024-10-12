b51f6b9316ddacf42de8402fda29f5b5515b607a
Marcel Lee <chungchung1315@gmail.com>
2024-10-12 12:19:42 +0900

    chore: Init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Our Company Lunch WebFlux
+
---
+++ src/main/java/com/marceldev/ourcompanylunchwebflux/OurCompanyLunchWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.marceldev.ourcompanylunchwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurCompanyLunchWebfluxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OurCompanyLunchWebfluxApplication.class, args);
+  }
+
+}
